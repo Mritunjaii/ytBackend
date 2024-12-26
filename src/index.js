1fd216fffbe2fb857dccea312f5867f9269c0615
Mritunjaii <themritunjai@gmail.com>
2024-12-26 16:17:49 +0530

    user and video model setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import express from "express";
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 import connectDB from "./db/index.js";
-
-
-connectDB();
-
-const app=express();
+import {app} from "./app.js";
 
 
 
 
-const port = process.env.PORT || 8000;
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-})
+const port = process.env.PORT || 3000;
+
+connectDB().then(()=>{
+    app.on("err",()=>{
+        console.log("Erron on listen:",err);
+        throw err;
+    })
+    app.listen(port,()=>{
+        console.log(`Server running on port ${port}`);
+    })
+}).catch((err)=>{
+    console.log("Failed Mongo connection in index app file:\n",err);
+});
+
+
